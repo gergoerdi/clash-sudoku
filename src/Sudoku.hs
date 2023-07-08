a2e83a7cba5e04d3d1cdbfc5ea8f939293db678e
Gergo ERDI <gergo@erdi.hu>
2023-07-08 11:06:15 +0800

    Solver, part 1: without backtracking

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Sudoku where
 
 import Clash.Prelude
 import Data.Bits
 import Data.Char (isDigit)
 import Clash.Sized.Vector (unsafeFromList)
+import Control.Monad (guard)
 
 type Matrix n m a = Vec n (Vec m a)
 type Square n = BitVector n
 newtype Sudoku n m = Sudoku{ getSudoku :: Matrix (n * m) (m * n) (Square (n * m)) }
 
 wild :: (KnownNat n) => BitVector n
-wild = 0
+wild = maxBound
 
 oneHot :: (KnownNat n) => Index n -> BitVector n
 oneHot n = 1 `shiftL` fromIntegral n
@@ -36,7 +40,8 @@ getUnique = fold propagate . zipWith start (reverse indicesI) . bitCoerce
 
 showSquare :: (KnownNat n, n <= 8) => Square (n + 1) -> Char
 showSquare x = case getUnique x of
-    Unset -> '_'
+    _ | x == maxBound -> '_'
+    Unset -> '?'
     Unique x -> ('1' :> '2' :> '3' :> '4' :> '5' :> '6' :> '7' :> '8' :> '9' :> Nil) !! x
     Conflict -> 'x'
 
@@ -55,6 +60,9 @@ showBoard
     => Sudoku n m -> String
 showBoard = unlines . fmap toList . toList . showBoard'
 
+instance (KnownNat n, KnownNat m, KnownNat k, (n * m) ~ (k + 1), k <= 8) => Show (Sudoku n m) where
+    show = showBoard
+
 readBoard'
     :: (KnownNat n, KnownNat m, KnownNat k, (n * m) ~ (k + 1), k <= 8)
     => Vec ((n * m) * (n * m)) (Square (n * m))
@@ -66,8 +74,23 @@ readBoard
     => String -> Sudoku n m
 readBoard = readBoard' . map readSquare . unsafeFromList
 
-board :: Sudoku 3 3
-board = readBoard . filter isDigit . unlines $
+board1 :: Sudoku 3 3
+board1 = readBoard . filter isDigit . unlines $
+    [ "0 2 0  9 0 8  0 0 0"
+    , "8 7 0  0 0 1  0 5 4"
+    , "5 0 6  4 0 0  0 1 0"
+    , ""
+    , "0 0 2  0 0 0  0 9 5"
+    , "0 0 0  0 0 0  0 0 0"
+    , "9 4 0  0 0 0  8 0 0"
+    , ""
+    , "0 8 0  0 0 4  5 0 3"
+    , "1 3 0  2 0 0  0 8 6"
+    , "0 0 0  3 0 7  0 2 0"
+    ]
+
+board2 :: Sudoku 3 3
+board2 = readBoard . filter isDigit . unlines $
     [ "0 0 0  6 0 0  5 0 0"
     , "0 6 0  0 0 8  0 4 0"
     , "0 0 0  7 0 4  0 0 0"
@@ -81,3 +104,59 @@ board = readBoard . filter isDigit . unlines $
     , "0 5 1  0 0 7  0 0 0"
     ]
 
+rowwise
+    :: (KnownNat n, KnownNat m, 1 <= n, Applicative f)
+    => (Vec m a -> f (Vec m b))
+    -> Matrix n m a
+    -> f (Matrix n m b)
+rowwise f = traverse f
+
+columnwise
+    :: (KnownNat n, KnownNat m, 1 <= m, Applicative f)
+    => (Vec n a -> f (Vec n b))
+    -> Matrix n m a
+    -> f (Matrix n m b)
+columnwise f = fmap transpose . traverse f . transpose
+
+toSquares
+    :: (KnownNat n, KnownNat m)
+    => Matrix (n * m) (m * n) a
+    -> Matrix n m (Vec (n * m) a)
+toSquares = map (map concat . transpose . map unconcatI) . unconcatI
+
+fromSquares
+    :: (KnownNat n, KnownNat m)
+    => Matrix n m (Vec (n * m) a)
+    -> Matrix (n * m) (m * n) a
+fromSquares = concat . map (map concat . transpose . map unconcatI)
+
+squarewise
+    :: forall n m. (KnownNat n, KnownNat m, 1 <= n, 1 <= m)
+    => forall f a b. (Applicative f)
+    => (Vec (n * m) a -> f (Vec (n * m) b))
+    -> Matrix (n * m) (m * n) a
+    -> f (Matrix (n * m) (m * n) b)
+squarewise f = fmap fromSquares . traverse (traverse f) . toSquares @n @m
+
+others :: (1 <= n) => Vec n a -> Vec n (Vec (n - 1) a)
+others (Cons x Nil) = Nil :> Nil
+others (Cons x xs@(Cons _ _)) = xs :> map (x :>) (others xs)
+
+simplify :: forall n k k0. (KnownNat n, 1 <= n, KnownNat k, k ~ k0 + 1) => Vec n (Square k) -> Maybe (Vec n (Square k))
+simplify xs = traverse (uncurry simplifySquare) (zip xs (others xs))
+  where
+    simplifySquare :: forall n. Square k -> Vec n (Square k) -> Maybe (Square k)
+    simplifySquare x xs = x' <$ guard (x' /= 0)
+      where
+        x' = foldl f x xs
+        f x y | Unique{} <- getUnique y = x .&. complement y
+              | otherwise = x
+
+propagate1
+    :: forall n m k. (KnownNat n, KnownNat m, 1 <= n, 1 <= m, KnownNat k, (n * m) ~ (k + 1))
+    => Sudoku n m -> Maybe (Sudoku n m)
+propagate1 s = do
+    (s :: Sudoku n m) <- fmap Sudoku . rowwise simplify . getSudoku $ s
+    (s :: Sudoku n m) <- fmap Sudoku . columnwise simplify . getSudoku $ s
+    (s :: Sudoku n m) <- fmap Sudoku . squarewise @n @m simplify . getSudoku $ s
+    return s
