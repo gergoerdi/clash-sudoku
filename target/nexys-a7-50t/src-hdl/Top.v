f8e3d90bd29301f51ba778920dcd663bb619e18f
Gergo ERDI <gergo@erdi.hu>
2023-07-11 20:24:49 +0800

    Add HDL stubs and Shakefile for Nexys A7-50T

---
+++ target/nexys-a7-50t/src-hdl/Top.v
@@ -0,0 +1,14 @@
+module Sudoku(
+           input        CLK100MHZ,
+           input        UART_TXD_IN,
+           output       UART_RXD_OUT
+           );
+
+   topEntity u_topEntity
+     (.CLK_100MHZ(CLK100MHZ),
+      .RESET(1'b0),
+      .RX(UART_TXD_IN),
+      .TX(UART_RXD_OUT)
+      );
+
+endmodule
