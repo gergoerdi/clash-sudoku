cd0d7a60903aca6c7dc5570701ed3d959766e049
Gergo ERDI <gergo@erdi.hu>
2023-07-13 17:03:50 +0800

    stack delay fixes

--- src/Sudoku/Stack.hs
+++ src/Sudoku/Stack.hs
@@ -10,7 +10,7 @@ data StackCmd a
     deriving (Show, Generic, NFDataX)
 
 stack
-    :: forall n a. (KnownNat n, NFDataX a)
+    :: forall n a. (KnownNat n, NFDataX a, 1 <= n)
     => forall dom. (HiddenClockResetEnable dom)
     => SNat n
     -> a
@@ -18,14 +18,14 @@ stack
     -> ( Signal dom (Maybe a)
        , Signal dom Bool
        )
-stack size x0 cmd = (enable (delay False en) rd, underflow)
+stack size x0 cmd = (enable (delay False en) rd, delay False underflow)
   where
     sp = register (0 :: Index n) sp'
     (sp', en, wr, underflow) = unbundle $ interpret <$> sp <*> cmd
 
     interpret sp cmd = case cmd of
         Nothing -> (sp, False, Nothing, False)
-        Just Pop -> (sp - 1, True, Nothing, sp == 0)
+        Just Pop -> (satPred SatBound sp, True, Nothing, sp == 0)
         Just (Push x) -> (sp + 1, False, Just x, False)
 
-    rd = blockRam (replicate size x0) sp' (packWrite <$> sp' <*> wr)
+    rd = blockRam (replicate size x0) sp (packWrite <$> sp' <*> wr)
