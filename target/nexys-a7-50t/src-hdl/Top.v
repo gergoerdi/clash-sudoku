822fcaab170769bf3767320a90480ace849c18f5
Gergo ERDI <gergo@erdi.hu>
2023-07-14 17:55:31 +0800

    Hook up `CPU_RESETN` button

--- target/nexys-a7-50t/src-hdl/Top.v
+++ target/nexys-a7-50t/src-hdl/Top.v
@@ -1,12 +1,13 @@
 module Top(
-           input        CLK100MHZ,
-           input        UART_TXD_IN,
-           output       UART_RXD_OUT
+           input  CLK100MHZ,
+           input  CPU_RESETN,
+           input  UART_TXD_IN,
+           output UART_RXD_OUT
            );
 
    topEntity u_topEntity
      (.CLK_100MHZ(CLK100MHZ),
-      .RESET(1'b0),
+      .RESET(~CPU_RESETN),
       .RX(UART_TXD_IN),
       .TX(UART_RXD_OUT)
       );
