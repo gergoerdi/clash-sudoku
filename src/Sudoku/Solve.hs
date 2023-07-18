a4bdc26bfa231622a6374a089b1de321570afd8b
Gergo ERDI <gergo@erdi.hu>
2023-07-18 12:26:47 +0800

    Factor out `neighbours`

--- src/Sudoku/Solve.hs
+++ src/Sudoku/Solve.hs
@@ -55,8 +55,8 @@ propagator load = (bundle $ fmap (\(c, _, _, _) -> c) units, result)
             | not changed -> Just Stuck
             | otherwise -> Nothing
 
-    unit :: Coord n m -> (Signal dom (Cell n m), Signal dom Bool, Signal dom Bool, Signal dom Bool)
-    unit idx@(i, j, k, l) = (r, isUnique <$> r, register False changed, (== conflicted) <$> r)
+    unit :: Coord n m -> (Signal dom (Space n m), Signal dom Bool, Signal dom Bool, Signal dom Bool)
+    unit idx = (r, isUnique <$> r, register False changed, (== conflicted) <$> r)
       where
         r :: Signal dom (Cell n m)
         r = register conflicted r'
@@ -64,28 +64,16 @@ propagator load = (bundle $ fmap (\(c, _, _, _) -> c) units, result)
         (r', changed) = unbundle do
             load <- load
             this <- r
-            row_masks <- traverse neighbour row'
-            col_masks <- traverse neighbour col'
-            box_masks <- traverse neighbour box'
+            masks <- traverse neighbour (neighbours idx)
             pure $ case load of
                 Just new_grid -> (gridAt new_grid idx, True)
                 Nothing -> (this', this' /= this)
                   where
-                    this' = combine this (row_masks ++ col_masks ++ box_masks)
+                    this' = combine this masks
 
-        neighbour idx = mux is_unique value (pure conflicted)
+        neighbour idx' = mux is_unique value (pure conflicted)
           where
-            (value, is_unique, _, _) = gridAt units idx
-
-        row, col, box :: Vec (n * m) (Coord n m)
-        row = concatMap (\k -> map (\l -> (i, j, k, l)) indicesI) indicesI
-        col = concatMap (\i -> map (\j -> (i, j, k, l)) indicesI) indicesI
-        box = concatMap (\j -> map (\l -> (i, j, k, l)) indicesI) indicesI
-
-        row', col', box' :: Vec ((n * m) - 1) (Coord n m)
-        row' = unconcatI (others row) !!! k !!! l
-        col' = unconcatI (others col) !!! i !!! j
-        box' = unconcatI (others box) !!! j !!! l
+            (value, is_unique, _, _) = gridAt cells idx'
 
 controller
     :: forall n m dom k. (KnownNat n, KnownNat m, 1 <= n, 1 <= m, 2 <= n * m, n * m * m * n ~ k + 1)
@@ -112,7 +100,3 @@ controller load = (grid, solved, stack_cmd)
         mux (result .== Just Stuck .&&. can_try) (Just . Push <$> after) $
         mux (result .== Just Failure) (pure $ Just Pop) $
         pure Nothing
-
-others :: (1 <= n) => Vec n a -> Vec n (Vec (n - 1) a)
-others (Cons x Nil) = Nil :> Nil
-others (Cons x xs@(Cons _ _)) = xs :> map (x :>) (others xs)
