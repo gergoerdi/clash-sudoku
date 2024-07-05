82f6e4b1b2a29c5ad2b61e1006d4c8d2dc33d165
Gergo ERDI <gergo@erdi.hu>
2024-07-05 22:12:02 +0800

    Compute guess candidates cell by cell, inside `propagator`

--- src/Sudoku/Solve.hs
+++ src/Sudoku/Solve.hs
@@ -2,15 +2,18 @@
 {-# LANGUAGE ApplicativeDo #-}
 module Sudoku.Solve (controller) where
 
-import Clash.Prelude
+import Clash.Prelude hiding (mapAccumR)
 import RetroClash.Utils
 
+import Sudoku.Hacks
 import Sudoku.Matrix
 import Sudoku.Grid
 import Sudoku.Stack
 
 import Control.Arrow (second, (***))
 import Data.Maybe
+
+import Debug.Trace
 
 foldGrid :: (n * m * m * n ~ k + 1) => (a -> a -> a) -> Grid n m a -> a
 foldGrid f = fold f . flattenGrid
@@ -50,44 +53,67 @@ data PropagatorResult
     = Solved
     | Failure
     | Stuck
+    | Guess
     | Progress
     deriving (Generic, NFDataX, Eq, Show)
 
 propagator
     :: forall n m dom k. (KnownNat n, KnownNat m, 1 <= n, 1 <= m, 1 <= n * m, n * m * m * n ~ k + 1)
     => (HiddenClockResetEnable dom)
-    => Grid n m (Signal dom (Maybe (Cell n m)))
+    => Signal dom (Maybe (Cell n m))
+    -> Signal dom (Maybe (Sudoku n m))
     -> ( Signal dom PropagatorResult
        , Grid n m (Signal dom (Cell n m))
+       , Signal dom Bool
+       , Grid n m (Signal dom (Cell n m))
        )
-propagator loads = (result, grid)
+propagator shift_in pop = (result, grid, can_guess, next_guesses)
   where
-    units@(unzipGrid -> (grid, changeds)) = unit <$> loads <*> neighbourMasks grid uniques
+    pops :: Grid n m (Signal dom (Maybe (Cell n m)))
+    pops = unbundle . fmap sequenceA $ pop
+
+    ((shift_out, keep_guessing), unzip3 -> (unflattenGrid -> grid, changeds, unflattenGrid -> next_guesses)) =
+        mapAccumR unit (shift_in, should_guess) (flattenGrid $ ((,) <$> pops <*> neighbourMasks grid uniques))
+
     uniques = fmap (isUnique <$>) grid
     faileds = fmap (.== conflicted) grid
 
-    fresh = register False $ foldGrid (.||.) $ fmap (isJust <$>) loads
+    should_guess = not <$> any_changed
+    can_guess = should_guess .&&. (not <$> keep_guessing)
+
+    fresh = register False $ isJust <$> shift_in .||. isJust <$> pop
     all_unique = foldGrid (.&&.) uniques
-    any_changed = foldGrid (.||.) changeds
+    any_changed = fold (.||.) changeds
     any_failed  = foldGrid (.||.) faileds
 
     result =
         mux fresh (pure Progress) $
         mux all_unique (pure Solved) $
         mux any_failed (pure Failure) $
-        mux (not <$> any_changed) (pure Stuck) $
+        mux can_guess (pure Guess) $
+        mux should_guess (pure Stuck) $
         pure Progress
 
     unit
         :: forall k. (KnownNat k)
-        => Signal dom (Maybe (Cell n m))
-        -> Vec k (Signal dom (Mask n m))
-        -> (Signal dom (Cell n m), Signal dom Bool)
-    unit load neighbour_masks = (r, changed)
+        => (Signal dom (Maybe (Cell n m)), Signal dom Bool)
+        -> (Signal dom (Maybe (Cell n m)), Vec k (Signal dom (Mask n m)))
+        -> ((Signal dom (Maybe (Cell n m)), Signal dom Bool), (Signal dom (Cell n m), Signal dom Bool, Signal dom (Cell n m)))
+    unit (shift_in, try_guess) (pop, neighbour_masks) = ((shift_out, keep_guessing), (r, changed, cont))
       where
+        load = shift_in .<|>. pop
+        shift_out = enable (isJust <$> shift_in) r
+
         r = register conflicted r'
-        r' = load .<|. propagate r neighbour_masks
+        r' = load .<|>. enable guess_this first_guess .<|. propagate r neighbour_masks
         changed = register False $ r ./=. r'
+
+        (first_guess, next_guess) = unbundle $ mux try_guess (splitCell <$> r) (bundle (r, r))
+
+        can_guess = next_guess ./= conflicted
+        guess_this = try_guess .&&. can_guess
+        cont = mux guess_this next_guess r
+        keep_guessing = try_guess .&&. (not <$> guess_this)
 
 controller
     :: forall n m dom k. (KnownNat n, KnownNat m, 1 <= n, 1 <= m, 1 <= n * m, n * m * m * n ~ k + 1)
@@ -98,25 +124,16 @@ controller
        )
 controller new_board = (enable (result .== Solved) (bundle grid), stack_cmd)
   where
-    (result, grid) = propagator loads
-
-    (can_try, unzipGrid -> (bundle -> next, bundle -> after)) = mapAccumGridB (liftA2 f) (pure False) grid
-      where
-        f :: Bool -> Cell n m -> (Bool, (Cell n m, Cell n m))
-        f found c = (found || this, if this then guess else (c, c))
-          where
-            guess@(_next, after) = splitCell c
-            this = not found && after /= conflicted
-
-    load = new_board .<|>. enable (can_try .&&. register False (result .== Stuck)) next
-    loads = unbundle . fmap sequenceA $ load
+    (result, grid, can_guess, next_guesses) = propagator (pure Nothing) load
+    load = new_board -- .<|>. enable (can_try .&&. register False (result .== Stuck)) next
 
     stack_cmd = do
         result <- result
-        can_try <- can_try
-        after <- after
+        can_guess <- can_guess
+        next_guesses <- bundle next_guesses
         pure $ case result of
             Solved -> Nothing
-            Stuck -> Just $ if can_try then Push after else Pop
+            Guess -> Just $ Push next_guesses
+            Stuck -> Just Pop
             Failure -> Just Pop
             Progress -> Nothing
