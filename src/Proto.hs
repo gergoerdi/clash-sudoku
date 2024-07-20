083bc6face51736e8746e8e8e52a308d279e2675
Gergo ERDI <gergo@erdi.hu>
2024-07-20 13:52:47 +0800

    Collapse composed punctuations

--- src/Proto.hs
+++ src/Proto.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE BlockArguments, LambdaCase, TupleSections #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving, DerivingStrategies, StandaloneDeriving #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module Proto where
 
 import Clash.Prelude
 import Clash.Class.Counter
+import Clash.Class.Counter.Internal
 import Protocols
 import qualified Protocols.Df as Df
 import qualified Data.List as L
@@ -55,34 +58,34 @@ test = simulateN @System 30 (bundle . uncurry (daisyChain (SNat @10)) . unbundle
   where
     ops = L.replicate 14 (Just $ Add 1) <> L.repeat Nothing
 
-data PuncState
-    = Forward
-    | Punctuate
-    deriving (Generic, NFDataX)
+-- data PuncState
+--     = Forward
+--     | Punctuate
+--     deriving (Generic, NFDataX)
 
-punctuate' :: (HiddenClockResetEnable dom) => b -> Circuit (Df dom a) (Df dom (Either b a))
-punctuate' sep = Circuit $ mealyB step Forward
-  where
-    step Forward (Df.Data input, ~(Ack out_ack)) = (if out_ack then Punctuate else Forward, (Ack out_ack, Df.Data (Right input)))
-    step Forward (Df.NoData, out_ack) = (Forward, (Ack False, Df.NoData))
-    step Punctuate (_, ~(Ack out_ack)) = (if out_ack then Forward else Punctuate, (Ack False, Df.Data (Left sep)))
+-- punctuate' :: (HiddenClockResetEnable dom) => b -> Circuit (Df dom a) (Df dom (Either b a))
+-- punctuate' sep = Circuit $ mealyB step Forward
+--   where
+--     step Forward (Df.Data input, ~(Ack out_ack)) = (if out_ack then Punctuate else Forward, (Ack out_ack, Df.Data (Right input)))
+--     step Forward (Df.NoData, out_ack) = (Forward, (Ack False, Df.NoData))
+--     step Punctuate (_, ~(Ack out_ack)) = (if out_ack then Forward else Punctuate, (Ack False, Df.Data (Left sep)))
 
-punctuate :: (HiddenClockResetEnable dom) => b -> Circuit (Df dom a) (Df dom (Either b a))
-punctuate sep = Df.expander Forward \ case
-    Punctuate -> \_ -> (Forward, Left sep, True)
-    Forward -> \x -> (Punctuate, Right x, False)
+-- punctuate :: (HiddenClockResetEnable dom) => b -> Circuit (Df dom a) (Df dom (Either b a))
+-- punctuate sep = Df.expander Forward \ case
+--     Punctuate -> \_ -> (Forward, Left sep, True)
+--     Forward -> \x -> (Punctuate, Right x, False)
 
-genPunctuateInput :: H.Gen [Int]
-genPunctuateInput = Gen.list (Range.linear 0 100) (genInt 10 20)
-  where
-    genInt a b = Gen.integral (Range.linear a b)
+-- genPunctuateInput :: H.Gen [Int]
+-- genPunctuateInput = Gen.list (Range.linear 0 100) (genInt 10 20)
+--   where
+--     genInt a b = Gen.integral (Range.linear a b)
 
-punctuateModel :: b -> [a] -> [Either b a]
-punctuateModel sep = go
-  where
-    go = \case
-        [] -> []
-        (x:xs) -> Right x : Left sep : go xs
+-- punctuateModel :: b -> [a] -> [Either b a]
+-- punctuateModel sep = go
+--   where
+--     go = \case
+--         [] -> []
+--         (x:xs) -> Right x : Left sep : go xs
 
 -- data Punctuate (n :: Nat) (sep :: Char) a
 --     = Forwarding (Index n)
@@ -120,39 +123,12 @@ instance (SymbolLength_ s, KnownChar c, KnownNat (SymbolLength s)) => SymbolLeng
 type SymbolLength s = SymbolLength' (UnconsSymbol s)
 type SymbolLength_ s = SymbolLength_' (UnconsSymbol s)
 
--- data Punctuate (rep :: Nat) (sep :: Symbol) a
---     = Position (Index rep) (Either (Index (SymbolLength sep)) a)
---     deriving (Show, Eq)
-
--- type PrettyGrid =
---     Punctuate 2 "\r\n" (
---     Punctuate 2 "\r\n" (
---     Punctuate 2 " " (
---     Punctuate 2 " "
---     ())))
-
--- startPrettyGrid :: PrettyGrid
--- startPrettyGrid =
---   Position 0 . Left $
---   Position 0 . Left $
---   Position 0 . Left $
---   Position 0 . Left $
---   ()
-
-
--- punctuation :: forall rep sep a. (SymbolLength_ sep) => Punctuate rep sep a -> Either Char a
--- punctuation (Position _ (Left i)) = Left $ symbolAt (Proxy @(UnconsSymbol sep)) i
--- punctuation (Position _ (Right x)) = Right x
-
-
-
 newtype Punctuate (rep :: Nat) (sep :: Symbol) a
     = MkPunctuate (Index rep, Either a (Index (SymbolLength sep)))
-    deriving (Show)
+    deriving stock (Show, Generic)
+    deriving newtype (NFDataX)
 
 deriving newtype instance (SymbolLength_ sep, KnownNat (SymbolLength sep), 1 <= SymbolLength sep, KnownNat rep, 1 <= rep, Counter a) => Counter (Punctuate rep sep a)
-
--- instance Counter ()
 
 type PrettyGrid =
     Punctuate 2 "\r\n" (
@@ -169,21 +145,39 @@ startPrettyGrid =
   MkPunctuate . (0,) . Left $
   0
 
-punctuation :: forall rep sep a. (SymbolLength_ sep) => Punctuate rep sep a -> Either Char a
-punctuation (MkPunctuate (_, Right i)) = Left $ symbolAt (Proxy @(UnconsSymbol sep)) i
-punctuation (MkPunctuate (_, Left x)) = Right x
+class Punctuating c a {- | a -> c -} where
+    punctuation :: a -> Maybe c
+
+instance (Punctuating c (Index n)) where
+    punctuation _ = Nothing
+
+instance (Punctuating Char k, SymbolLength_ sep) => Punctuating Char (Punctuate rep sep k) where
+    punctuation = \case
+        MkPunctuate (_, Right i) -> Just $ symbolAt (Proxy @(UnconsSymbol sep)) i
+        MkPunctuate (_, Left k) -> punctuation k
+
+-- punctuate :: forall dom rep sep a b. _ => Punctuate rep sep b -> Circuit (Df dom a) (Df dom (Either Char (a, b)))
+-- punctuate spec = Df.expander spec \s ->
+--     case punctuation s of
+--         Left sep -> \_ -> (s', Left sep, overflow)
+--           where
+--             (overflow, s') = countSuccOverflow s
+--         Right y -> \x -> (countSucc s, Right (x, y), False)
 
 foo :: [Char] -> [Char]
 foo = go startPrettyGrid
   where
     go :: PrettyGrid -> [Char] -> [Char]
-    go spec cs = case (punctuation >=> punctuation >=> punctuation >=> punctuation) spec of
-        Left sep -> sep : go spec' cs
-        Right _ -> case cs of
+    go spec cs = case punctuation spec of
+        Just sep -> sep : go spec' cs
+        Nothing -> case cs of
             c:cs' -> c : go spec' cs'
             [] -> []
       where
         spec' = countSucc spec
+
+-- fooCirc :: _ => Circuit (Df dom a) (Df dom (Either Char a))
+-- fooCirc = punctuate (
 
 type T = Punctuate 2 "2" (Punctuate 2 "1" (Index 1))
 
@@ -191,9 +185,9 @@ foo2 :: [Char] -> [(T, Char)]
 foo2 = go (MkPunctuate . (0,) . Left $ MkPunctuate . (0,) . Left $ 0)
   where
     go :: T -> [Char] -> [(T, Char)]
-    go spec@spec0 cs = case (punctuation >=> punctuation) spec of
-        Left sep -> (spec0, sep) : go spec' cs
-        Right _ -> case cs of
+    go spec@spec0 cs = case punctuation spec of
+        Just sep -> (spec0, sep) : go spec' cs
+        Nothing -> case cs of
                 c:cs' -> (spec0, c) : go spec' cs'
                 [] -> []
       where
@@ -212,10 +206,10 @@ prettyGridModel (a0:a1:a2:a3:b0:b1:b2:b3:c0:c1:c2:c3:d0:d1:d2:d3:_) =
 blah :: [Char]
 blah = ['a'..'z']
 
-prop_punctuate :: (NFData b, Show b, Eq b, ShowX b, NFDataX b) => b -> H.Property
-prop_punctuate sep =
-  H.idWithModelSingleDomain
-    H.defExpectOptions
-    genPunctuateInput
-    (\_ _ _ -> punctuateModel sep)
-    (exposeClockResetEnable $ punctuate @System sep)
+-- prop_punctuate :: (NFData b, Show b, Eq b, ShowX b, NFDataX b) => b -> H.Property
+-- prop_punctuate sep =
+--   H.idWithModelSingleDomain
+--     H.defExpectOptions
+--     genPunctuateInput
+--     (\_ _ _ -> punctuateModel sep)
+--     (exposeClockResetEnable $ punctuate @System sep)
