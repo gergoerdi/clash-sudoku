1d4ab74dc2e03dea47469e4acff9edfac730e7d2
Gergo ERDI <gergo@erdi.hu>
2024-07-21 13:20:47 +0800

    fancier standalone

--- src/Sudoku4.hs
+++ src/Sudoku4.hs
@@ -20,14 +20,12 @@ import Clash.Cores.UART(uart, ValidBaud)
 ascii :: Char -> Unsigned 8
 ascii = fromIntegral . ord
 
-byteCircuit
+punctuate
     :: (HiddenClockResetEnable dom)
     => Circuit (Df dom (Unsigned 8)) (Df dom (Unsigned 8))
-byteCircuit = punctuate
-  where
-    punctuate = Df.expander False \case
-        False -> \x -> (True, x, False)
-        True -> \_ -> (False, ascii ',', True)
+punctuate = Df.expander False \case
+    False -> \x -> (True, x, False)
+    True -> \_ -> (False, ascii ',', True)
 
 -- From git@github.com:bittide/bittide-hardware.git
 uartDf ::
@@ -39,25 +37,6 @@ uartDf ::
 uartDf baud = Circuit \((request, rx_bit), _out_ack) ->
     let (received, tx_bit, in_ack) = uart baud rx_bit (Df.dataToMaybe <$> request)
     in ((Ack <$> in_ack, pure ()), (received, tx_bit))
-
-byteCircuit'
-    :: (HiddenClockResetEnable dom)
-    => ( Signal dom (Maybe (Unsigned 8))
-       , Signal dom Bool
-       )
-    -> ( Signal dom Bool
-       , Signal dom (Maybe (Unsigned 8))
-       )
-byteCircuit' (in_byte, out_ack) = (in_ack, out_byte)
-  where
-    (in_ack', out_byte') = toSignals byteCircuit (in_byte', out_ack')
-
-    in_byte' = Df.maybeToData <$> in_byte
-    out_ack' = Ack <$> out_ack
-    in_ack = (\case { Ack b -> b }) <$> in_ack'
-    out_byte = Df.dataToMaybe <$> out_byte'
-
-sim_byteCircuit = simulateCSE (exposeClockResetEnable $ byteCircuit @System)
 
 model_encodeSerial :: Int -> Unsigned 8 -> [Bit]
 model_encodeSerial stretch x = mconcat
@@ -108,29 +87,45 @@ buffer = Circuit \(x, ack) ->
             pure $ if ack then next else current <|> next
     in (pure (), Df.maybeToData <$> r)
 
+board :: (HiddenClockResetEnable System) => Bool -> Bool -> Signal System Bit -> Signal System Bit
+board buffer_input apply_punctuation rx = tx
+  where
+    ((out_ack, _), (in_bv, tx)) = toSignals (uartDf (SNat @5_000_000)) ((fmap pack <$> out_byte, rx), (pure (), pure ()))
+
+    in_byte = fmap unpack <$> in_bv
+
+    in_byte'
+        | buffer_input
+        = let (_, in_byte') = toSignals buffer (in_byte, out_ack) in in_byte'
+
+        | otherwise
+        = Df.maybeToData <$> in_byte
+
+    out_byte :: Signal System (Df.Data (Unsigned 8))
+    out_byte
+        | apply_punctuation
+        = let (in_ack, out_byte) = toSignals punctuate (in_byte', out_ack) in out_byte
+
+        | otherwise
+        = in_byte'
+
 topEntity
     :: "CLK_100MHZ" ::: Clock System
     -> "RESET"      ::: Reset System
     -> "ENABLE"     ::: Enable System
     -> "RX"         ::: Signal System Bit
     -> "TX"         ::: Signal System Bit
-topEntity clk rst en = withClockResetEnable clk rst en sudoku
-  where
-    sudoku rx = tx
-      where
-        ((out_ack, _), (in_bv, tx)) = toSignals (uartDf (SNat @5_000_000)) ((fmap pack <$> out_byte, rx), (pure (), pure ()))
-
-        (_, in_byte) = toSignals (buffer |> Df.map unpack) (in_bv, out_ack)
-        -- in_byte = fmap unpack . Df.maybeToData <$> in_bv
-
-        out_byte :: Signal System (Df.Data (Unsigned 8))
-        (in_ack, out_byte) = toSignals byteCircuit (in_byte, out_ack)
-        -- out_byte = in_byte
+topEntity clk rst en = withClockResetEnable clk rst en (board True True)
 
 makeTopEntity 'topEntity
 
-sim =
-    L.map (chr . fromIntegral) .
+simParallel =
+    fmap (chr . fromIntegral) .
+    simulateCSE @System (exposeClockResetEnable punctuate) $
+    L.take 81 $ L.cycle . fmap ascii $ "Hello"
+
+simSerial =
+    fmap (chr . fromIntegral) .
     model_decodeSerial 20 .
     simulate @System (hideClockResetEnable topEntity) .
     model_encodeSerials 20 $
