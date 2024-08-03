ed7d4a530177a443b0d775bd810f06bf8211976c
Gergo ERDI <gergo@erdi.hu>
2024-08-03 19:53:29 +0800

    Separate stack command and stack data lines, to avoid big muxes

--- src/Sudoku/Stack.hs
+++ src/Sudoku/Stack.hs
@@ -16,18 +16,19 @@ stack
     => forall dom. (HiddenClockResetEnable dom)
     => SNat n
     -> a
-    -> Signal dom (Maybe (StackCmd a))
+    -> Signal dom (Maybe (StackCmd ()))
+    -> Signal dom a
     -> ( Signal dom (Maybe a)
        , Signal dom (Index n)
        )
-stack size x0 cmd = (enable (delay False en) rd, sp)
+stack size x0 cmd dat = (enable (delay False en) rd, sp)
   where
     sp = register 0 sp'
     (sp', en, wr) = unbundle $ interpret <$> sp <*> cmd
 
     interpret sp cmd = case cmd of
-        Nothing -> (sp, False, Nothing)
-        Just Pop -> (satPred SatWrap sp, True, Nothing)
-        Just (Push x) -> (satSucc SatWrap sp, False, Just x)
+        Nothing -> (sp, False, False)
+        Just Pop -> (satPred SatWrap sp, True, False)
+        Just (Push ()) -> (satSucc SatWrap sp, False, True)
 
-    rd = blockRamU NoClearOnReset size (\_ -> x0) sp (packWrite <$> sp' <*> wr)
+    rd = blockRamU NoClearOnReset size (\_ -> x0) sp (packWrite <$> sp' <*> (enable wr dat))
