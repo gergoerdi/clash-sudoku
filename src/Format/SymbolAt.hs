50edc6b7edc172c63ce93eb5bd557c6d8c0537ab
Gergo ERDI <gergo@erdi.hu>
2024-08-04 11:38:37 +0800

    Improve `SymbolAt` names

--- src/Format/SymbolAt.hs
+++ src/Format/SymbolAt.hs
@@ -1,20 +1,20 @@
 {-# LANGUAGE UndecidableInstances #-}
-module Format.SymbolAt where
+module Format.SymbolAt (SymbolLength, IndexableSymbol, symbolAt) where
 
 import Clash.Prelude
 import Data.Proxy
 
-class SymbolLength_' (s :: Maybe (Char, Symbol)) where
+class (KnownNat (SymbolLength' s)) => IndexableSymbol' (s :: Maybe (Char, Symbol)) where
     type SymbolLength' s :: Nat
 
     symbolAt :: proxy s -> Index (SymbolLength' s) -> Char
 
-instance SymbolLength_' Nothing where
+instance IndexableSymbol' Nothing where
     type SymbolLength' Nothing = 0
 
     symbolAt _ i = errorX "impossible"
 
-instance (SymbolLength_ s, KnownChar c, KnownNat (SymbolLength s)) => SymbolLength_' (Just '(c, s)) where
+instance (IndexableSymbol s, KnownChar c) => IndexableSymbol' (Just '(c, s)) where
     type SymbolLength' (Just '(c, s)) = 1 + SymbolLength s
 
     symbolAt _ i
@@ -25,4 +25,4 @@ instance (SymbolLength_ s, KnownChar c, KnownNat (SymbolLength s)) => SymbolLeng
         = symbolAt (Proxy @(UnconsSymbol s)) (fromIntegral (i - 1))
 
 type SymbolLength s = SymbolLength' (UnconsSymbol s)
-type SymbolLength_ s = SymbolLength_' (UnconsSymbol s)
+type IndexableSymbol s = IndexableSymbol' (UnconsSymbol s)
