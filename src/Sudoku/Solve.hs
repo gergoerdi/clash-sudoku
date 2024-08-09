1dbce5fbc619431213941526aea83ac9133b33cc
Gergo ERDI <gergo@erdi.hu>
2024-08-09 20:38:58 +0800

    Remove dead code

--- src/Sudoku/Solve.hs
+++ src/Sudoku/Solve.hs
@@ -24,12 +24,6 @@ import Debug.Trace
 foldGrid :: forall n m a. (1 <= n * m * m * n) => (a -> a -> a) -> Grid n m a -> a
 foldGrid f = fold @(n * m * m * n - 1) f . flattenGrid
 
-unzipMatrix :: Matrix n m (a, b) -> (Matrix n m a, Matrix n m b)
-unzipMatrix = (FromRows *** FromRows) . unzip . fmap unzip . matrixRows
-
-unzipGrid :: Grid n m (a, b) -> (Grid n m a, Grid n m b)
-unzipGrid = (Grid *** Grid) . unzipMatrix . fmap unzipMatrix . getGrid
-
 data PropagatorResult
     = Solved
     | Failure
