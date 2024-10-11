ec9e583c6bfa186cb5466cd2f192c1a58a8b6a19
Gergo ERDI <gergo@erdi.hu>
2024-10-11 18:51:44 +0800

    Much nicer `SaturatingNum Bit` instance

--- src/Sudoku/Utils.hs
+++ src/Sudoku/Utils.hs
@@ -5,6 +5,8 @@ module Sudoku.Utils where
 
 import Clash.Prelude
 import Data.Monoid (Any(..), All(..))
+import Data.Function (on)
+import Data.Composition
 
 enable :: (Applicative f) => f Bool -> f a -> f (Maybe a)
 enable en x = mux en (Just <$> x) (pure Nothing)
@@ -22,23 +24,6 @@ deriving via Bool instance BitPack All
 deriving via Bool instance BitPack Any
 
 instance SaturatingNum Bit where
-    satAdd = \case
-        SatWrap -> addOr 0
-        SatBound -> addOr maxBound
-        SatZero ->  addOr 0
-        SatSymmetric -> addOr maxBound
-        SatError -> addOr $ errorX "satAdd"
-      where
-        addOr ext = \x y -> if x == 1 && y == 1 then ext else x + y
-
-    satSub = \case
-        SatWrap -> subOr 1
-        SatBound -> subOr minBound
-        SatZero ->  subOr 0
-        SatSymmetric -> subOr maxBound
-        SatError -> subOr $ errorX "satSub"
-      where
-        subOr ext = \x y -> if x == 0 && y == 1 then ext else x - y
-
-    satMul = \case
-        _ -> (*)
+    satAdd mode = unpack .: satAdd mode `on` pack
+    satSub mode = unpack .: satSub mode `on` pack
+    satMul mode = unpack .: satMul mode `on` pack
