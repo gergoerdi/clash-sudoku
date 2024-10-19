804b5c272acd9710e4da6ffa106dae8f31465573
Gergo ERDI <gergo@erdi.hu>
2024-10-19 09:12:36 +0800

    Fiddle with the pure implementation

--- test/Sudoku/Pure.hs
+++ test/Sudoku/Pure.hs
@@ -14,19 +14,22 @@ import Data.Monoid (Any(..), All(..))
 import Data.Monoid.Action
 import Control.Monad (guard)
 
-propagate1 :: forall n m. (Solvable n m) => Sudoku n m -> Maybe (Sudoku n m)
+isUnique :: (Solvable n m) => Cell n m -> Bool
+isUnique cell = popCount (cellBits cell) == 1
+
+propagate1 :: (Solvable n m) => Sudoku n m -> Maybe (Sudoku n m)
 propagate1 grid = do
+    guard $ not (failed grid)
     masks' <- neighbourhoodMasks masks
     pure $ apply <$> uniques <*> masks' <*> grid
   where
     uniques = isUnique <$> grid
     masks = maskOf <$> uniques <*> grid
 
-    isUnique cell = cell == fst (splitCell cell)
     maskOf is_unique cell = if is_unique then cellMask cell else mempty
     apply is_unique mask = if is_unique then id else act mask
 
-propagate :: forall n m. (Solvable n m) => Sudoku n m -> Maybe (Sudoku n m)
+propagate :: (Solvable n m) => Sudoku n m -> Maybe (Sudoku n m)
 propagate grid = do
     grid' <- propagate1 grid
     let changed = grid' /= grid
@@ -40,6 +43,16 @@ possibilities cell =
     , cellBits cell .&. cellBits cell' /= 0
     ]
 
+possibilities1 :: (Solvable n m) => Cell n m -> [Cell n m]
+possibilities1 cell
+    | cont == conflicted
+    = []
+
+    | otherwise
+    = [guess, cont]
+  where
+    (guess, cont) = splitCell cell
+
 guess :: forall n m. (Solvable n m) => Sudoku n m -> [Sudoku n m]
 guess cells = do
     guard guessed
@@ -52,7 +65,7 @@ guess cells = do
     f :: Cell n m -> Bool -> (Bool, [Cell n m])
     f cell keep_guessing
         | keep_guessing
-        , cells@(_:_:_) <- possibilities cell
+        , cells@(_:_:_) <- possibilities1 cell
         = (False, cells)
 
         | otherwise
@@ -72,6 +85,5 @@ failed = getAny . reduceAny . fmap (== conflicted)
 
 search :: forall n m. (Solvable n m) => Sudoku n m -> [Sudoku n m]
 search grid = do
-    guard $ not (failed grid)
     grid <- maybeToList $ propagate grid
     if correct grid then pure grid else search =<< guess grid
